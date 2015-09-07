70b917775ef1c240d38eaf4afdd00d827723e3f4
Ganesh Datta <gdatta@ucsd.edu>
2015-09-06 23:22:57 -0700

    Update issues

--- setup.py
+++ setup.py
@@ -16,11 +16,6 @@ setuptools.setup(
     install_requires=[],
 
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
     ],
 )
